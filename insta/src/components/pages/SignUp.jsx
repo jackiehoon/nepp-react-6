2fecf91db49c44b8d30b573d47bb4aef153adb3b
jackiehoon <wognsdl4545@naver.com>
2022-08-16 22:23:42 +0900

    day:17 서버 셋팅

--- insta/src/components/pages/SignUp.jsx
+++ insta/src/components/pages/SignUp.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 import {
@@ -11,16 +12,52 @@ import {
   SignUpWrapper,
 } from "../atoms/logIn";
 
+import authApis from "../../apis/auth";
+
 const SignUp = () => {
+  const [form, setForm] = useState({
+    user_name: "",
+    password: "",
+    name: "",
+  });
+  const { user_name, password, name } = form;
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    const newForm = { ...form, [name]: value };
+    setForm(newForm);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    authApis.login(form);
+  };
+
   return (
     <Layout>
       <Main>
         <Box>
           <ImgLogo src="https://www.instagram.com/static/images/web/logged_out_wordmark.png/7a252de00b20.png" />
-          <Form>
-            <Input placeholder="사용자 이름" />
-            <Input placeholder="비밀번호" type="password" />
-            <Input placeholder="성명" />
+          <Form onSubmit={handleSubmit}>
+            <Input
+              placeholder="사용자 이름"
+              name="user_name"
+              onChange={handleChange}
+              value={user_name}
+            />
+            <Input
+              placeholder="비밀번호"
+              name="password"
+              type="password"
+              onChange={handleChange}
+              value={password}
+            />
+            <Input
+              placeholder="성명"
+              name="name"
+              onChange={handleChange}
+              value={name}
+            />
             <BtnSubmit>가입</BtnSubmit>
           </Form>
         </Box>
